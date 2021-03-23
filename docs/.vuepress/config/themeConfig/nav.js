29aacb7d566b9b7314dad133e0bd21743f4195b1
21haoxingxiu <1047918517@qq.com>
2021-03-23 17:17:58 +0800

    新增收集文章

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -58,8 +58,8 @@ module.exports = [
       { text: 'webpack', link: '/pages/0131de/' },
       { text: 'React', link: '/pages/a90c13/' },
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },   
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' }   
+      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
     ],
   },
   {
@@ -80,6 +80,7 @@ module.exports = [
       { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
       { text: '资源', link: '/pages/eee83a9211a70f9d/' },
       { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+      { text: '好文收藏', link: '/pages/d383f5/' },
     ],
   },
   {
